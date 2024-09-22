94a439d9b1b1c125e110ee9168db378f78495c9c
WANG Xu <feici02@outlook.com>
2024-09-22 11:02:50 +0800

    test ci

--- test.sh
+++ test.sh
@@ -7,4 +7,20 @@ EOF
     )
     echo "$text!"
 }
-hello "GitHub Actions"
+
+
+function post_to_feishu() {
+    local text="$1"
+    curl -X POST -H "Content-Type: application/json" \
+    -d @- https://open.feishu.cn/open-apis/bot/v2/hook/ead5417f-20b9-425c-8ce7-f4b8dd008deb <<EOF
+    {
+        "msg_type": "text",
+        "content": {
+        "text": "$text"
+        }
+    }
+EOF
+}
+
+PR_INFO="abc\n123"
+post_to_feishu "$PR_INFO"
